e6d13b630704e63f9b7892e32741603332157af6
eilincoln <lincoln.santospro@gmail.com>
2022-11-29 17:44:02 -0300

    add-javascript-perguntas

--- js/script.js
+++ js/script.js
@@ -23,5 +23,25 @@ function ativarProduto(parametro) {
     elemento.checked = true;
   }
 }
-
 parametro.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  console.log(ativa);
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
